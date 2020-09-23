0d0902d16c418de315d0e173d74498c8bba312ed
Angélica CarrMo <42524889+acarrmo@users.noreply.github.com>
2020-09-23 18:24:45 -0500

    Create ParImpar.java
    Calcula si es número par o impar, declarado, no solicita número desde teclado.

---
+++ ParImpar.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int numero = 7;
+        
+        
+        if (numero > 0)
+        {
+        
+            if (numero % 2 == 0)
+            {
+               System.out.println("El número introducido es par");
+            }
+        
+            else
+            {
+            System.out.println("El número introducido es impar");
+            }
+        }
+        
+        else
+        {
+            System.out.println("Introduce número positivo");
+        }
+        
+     
+    }
+}
